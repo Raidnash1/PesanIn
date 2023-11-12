a7b1534217693fa7ffc5a11cfe04b3d82c78e0c9
Raidnash1 <egbdfface56@gmail.com>
2023-11-12 14:45:54 +0700

    first commit

---
+++ resources/views/menus/index.blade.php
@@ -0,0 +1,182 @@
+<x-guest-layout>
+
+    <!-- ------------------------ Menu Main Content [Filter & Menu Card] Section ------------------------ -->
+    <section>
+        <div class="container" style="margin-bottom: 100px">
+            <div class="col-md-12">
+                <h3>Kelola Menu</h3>
+                <div class="btn-group my-4" role="group" aria-label="Basic radio toggle button group">
+                    <p class="my-auto mx-2"><b>Kategori</b></p>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                    <label class="btn btn-outline-warning" for="btnradio1">Semua</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                    <label class="btn btn-outline-warning" for="btnradio2">Makanan</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                    <label class="btn btn-outline-warning" for="btnradio3">Minuman</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
+                    <label class="btn btn-outline-warning" for="btnradio3">Jajanan</label>
+                </div>
+
+                <div class="row g-3">
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    v
+
+                    v
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="card card-borderless-shadow card-min-height">
+                            <img src="{{ url('images/menu/bakso-spesial.png') }}"
+                                class="card-img-top card-img-top-menus" />
+                            <div class="card-body">
+                                <p class="">Tahu Bulat</p>
+                                <p class="fw-semibold">Rp1.000,00</p>
+                                <button class="btn btn-success">Tersedia</button>
+                                <button class="btn btn-warning">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 my-auto">
+
+
+                        <div class="card card-borderless-shadow card-min-height p-4">
+
+                            <div class="card-body mx-auto mt-5 mb-5 p-5">
+                                <h1>+</h1>
+                            </div>
+                        </div>
+                        </a>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+        </div>
+        </div>
+    </section>
+
+</x-guest-layout>
